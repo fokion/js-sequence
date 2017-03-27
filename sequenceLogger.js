88ccf27bf880140e5151764422e08567bafef359
fokion <fokion@textensor.com>
2017-03-27 15:09:42 +0100

    Show the argument types and the sequence that can be visualized by https://bramp.github.io/js-sequence-diagrams/

--- sequenceLogger.js
+++ sequenceLogger.js
@@ -1,20 +1,39 @@
 window["LOGGER"] = {};
 window["LOGGER"]["log"] = true;
 window["LOGGER"]["sequence"] = [];
-window["LOGGER"]["logGenerator"] = function(callerName , functionName , notes){
-  return callerName + '->' + functionName;
-}
+window["LOGGER"]["prevElement"] = null;
+window["LOGGER"]["generateSequence"] = function(title){
+  var arr = ["Title : "+title];
+  var prev = null;
+  window["LOGGER"]["sequence"].forEach(function(element){
+    if(prev){
+      arr.push(prev["caller"] + " -> " + element["caller"]+" : "+element["func"]+" {"+element["args"].join(",")+"}");
+    }
+    prev = element;
+  });
+  return arr.join("\n");
+};
+window["LOGGER"]["logGenerator"] = function(callerName , functionName , arguments){
+ var args = [];
+ for(var i = 0 ; i < arguments.length ;i++){
+    args.push(typeof arguments[i]);
+  };
+  return {"caller":callerName,"func":functionName,"args":args};
+};
 window["LOGGER"]["getFunctionLogged"] = function(caller, func , name){
   return function() {
         if (window["LOGGER"]["log"] ) {
-            window["LOGGER"]["sequence"].push(window["LOGGER"]["logGenerator"](caller, name));
+            window["LOGGER"]["sequence"].push(window["LOGGER"]["logGenerator"](caller, name,arguments));
         }
-        func.apply(this, arguments);
+        return func.apply(this, arguments);
     }
 };
-window["LOGGER"]["addFunctionToLog"] = function(objectToRecord){
+window["LOGGER"]["addClassForLogging"] = function(objectToRecord){
   for(var name in objectToRecord){
     var functionToLog = objectToRecord[name];
+    if(Object.prototype.toString.call(functionToLog) === '[object Object]'){
+      window["LOGGER"]["addClassForLogging"](functionToLog);
+    }
     if(Object.prototype.toString.call(functionToLog) === '[object Function]'){
         objectToRecord[name] = window["LOGGER"].getFunctionLogged(objectToRecord.constructor.name , functionToLog, name);
     }
